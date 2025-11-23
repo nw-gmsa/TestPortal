3743745e4760cdac1af910a9d390b6a025488358
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2025-11-23 06:15:29 +0000

    PDF support

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  conformanceServer: 'https://nw-gmsa.github.io/TestPortal/fhir/r4',
+  fhirServer: 'https://nw-gmsa.github.io/TestPortal/fhir/r4',
+  tieServer: 'https://nw-gmsa.github.io/TestPortal/fhir/r4',
+  directoryServer: 'https://nw-gmsa.github.io/TestPortal/fhir/r4',
+  stravaClientId: '123',
+  stravaSecret: 'removedevsecretandreplaceitwithyourownstravasecretkeyhere',
+  withingClientId: 'removedevsecretandreplaceitwithyourownstravasecretkeyhere',
+  withingSecret: 'removedevsecretandreplaceitwithyourownstravasecretkeyhere',
+  addDataSupport: false,
+  hintsSupport: false,
+  physicalActivitySupport: false,
+  workflow : true,
+  pcc : false
+};
